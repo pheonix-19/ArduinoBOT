0c5638bacd278faac0c1541061068c2d79856b72
pheonix-19 <am2836166@gmail.com>
2025-06-14 22:46:02 +0530

    added the server and clinet

--- src/arduinobot_py_examples/setup.py
+++ src/arduinobot_py_examples/setup.py
@@ -22,7 +22,8 @@ setup(
         'console_scripts': [
             'simple_publisher = arduinobot_py_examples.simple_publisher:main',
             'simple_subscriber = arduinobot_py_examples.simple_subscriber:main',
-            'simple_parameter = arduinobot_py_examples.simple_parameter:main'
+            'simple_parameter = arduinobot_py_examples.simple_parameter:main',
+            'simple_service_server = arduinobot_py_examples.simple_service_server:main',
             
         ],
     },
