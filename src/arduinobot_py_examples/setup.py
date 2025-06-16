ab4de84c490c8c9e178687176754498820d5b3d9
pheonix-19 <am2836166@gmail.com>
2025-06-16 15:30:37 +0530

    added moveit api

--- src/arduinobot_py_examples/setup.py
+++ src/arduinobot_py_examples/setup.py
@@ -1,4 +1,7 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup
 
 package_name = 'arduinobot_py_examples'
 
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +28,8 @@ setup(
             'simple_subscriber = arduinobot_py_examples.simple_subscriber:main',
             'simple_parameter = arduinobot_py_examples.simple_parameter:main',
             'simple_service_server = arduinobot_py_examples.simple_service_server:main',
+            'simple_moveit_interface = arduinobot_py_examples.simple_moveit_interface:main'
+
             
         ],
     },
